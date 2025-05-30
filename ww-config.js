2887deb87a188adfcf25fa598c97976d00b24c1c
Ryan Leitch <LEITCRYAN@luther.vic.edu.au>
2025-05-30 23:46:37 +1000

    Convert to WeWeb-compliant Vue component with config

---
+++ ww-config.js
@@ -0,0 +1,18 @@
+export default {
+  editor: {
+    label: {
+      en: "Store Map",
+    },
+    icon: "map", // Optional: chooses an icon in WeWeb editor
+  },
+  properties: {
+    shapes: {
+      label: {
+        en: "Shapes",
+      },
+      type: "Array",
+      bindable: true,
+      defaultValue: [],
+    },
+  },
+};
